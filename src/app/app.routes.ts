784ea4e6aeee4373cec6b6f4f24d552ef65c1c74
bobytudu <js903783@gmail.com>
2024-08-07 19:39:50 +0530

    added new code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LayoutComponent } from './layout/layout.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'sign-up', component: SignUpComponent },
+    ],
+  },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
